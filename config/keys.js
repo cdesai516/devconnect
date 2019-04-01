46e2a0533bc2a7dad4cf494a1a3e849c83ac0ac0
chaitany desai <cdesai516@gmail.com>
2019-04-01 15:21:14 -0400

    frist commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://sean123:sean123@cluster0-trinn.mongodb.net/test?retryWrites=true"'
+};
---
+++ routes/api/users.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../../models/User.js");
+// @route   GET api/users/test
+// @desc    Tests users route
+// @access  Public
+router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already Exists" });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+      newUser.save().then(user => res.json(user));
+    }
+  });
+});
+module.exports = router;
